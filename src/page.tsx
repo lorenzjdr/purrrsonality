508121284c717eb9b07930f1e760d9f2f56b69c7
Olivia Laurel <zsw26@txstate.edu>
2024-10-17 13:47:37 -0500

    adding in page so there can be a start page

--- src/page.tsx
+++ src/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Quiz from "./components/Quiz";
 import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div className="Meow">
       <h1>Purrrsonality Quiz</h1>
+      <Quiz />
     </div>
   );
 };
